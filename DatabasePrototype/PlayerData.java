ef11d53389b08e4f44857a4d3edf7ec837d8d8c9
Patrick Ksor <71914119+Patar-spec@users.noreply.github.com>
2021-03-24 14:58:08 -0400

    included category

--- DatabasePrototype/PlayerData.java
+++ DatabasePrototype/PlayerData.java
@@ -6,20 +6,33 @@ public class PlayerData extends DataBasePrototype.DataObject {
 
     protected String playerName;
     protected String playerScore;
+    protected String category;
 
+
+
+
+    // ================================ GETTERS ====================================
+    public String getCategory() {
+        return category;
+    }
+    public String getPlayerName() {
+        return playerName;
+    }
     public String getPlayerScore() {
+
         return playerScore;
     }
+    // ================================ SETTERS ====================================
 
+    public void setCategory(String _category) {
+        this.category = _category;
+    }
     public void setPlayerScore(String _playerScore) {
         this.playerScore = _playerScore;
     }
-
     public void setPlayerName(String _playerName) {
         this.playerName = _playerName;
     }
 
-    public String getPlayerName() {
-        return playerName;
-    }
+
 }
