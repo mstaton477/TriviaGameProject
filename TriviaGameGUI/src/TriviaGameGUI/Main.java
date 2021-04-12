88ad590db564dc286b39e191cf2d0ec99b75c656
jtw5477 <70678051+jtw5477@users.noreply.github.com>
2021-04-12 14:25:29 -0400

    First bit of GUI
    What I had for our first code review, dumped the whole folder just in case something was needed.

---
+++ TriviaGameGUI/src/TriviaGameGUI/Main.java
@@ -0,0 +1,23 @@
+package TriviaGameGUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("TriviaGameMainView.fxml"));
+        primaryStage.setTitle("Trivia Game");
+        primaryStage.setScene(new Scene(root, 636.0, 468.0));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
