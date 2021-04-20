aa1518ba7c1e52665f757c886f3261c9c78d5c23
Thida <sslorvan@uncg.edu>
2021-04-19 22:49:18 -0400

    Reduced code and parsed answers
    Reduced code in Questions class and parsed incorrect answers

--- utils/APICode/Answers.java
+++ utils/APICode/Answers.java
@@ -2,76 +2,42 @@ package APICode;
 
 /**
  * Separates correct answers and incorrect answers from query.
- * Updated 4-19-21
+ * Updated 4-13-21
  *
  * @author Sengthida Lorvan
  */
 
 public class Answers extends Questions {
     public static String[] correctAnswer = new String[_questions];
-    public static String[] incorrectAnswer = new String[_questions];
 
     public static String[] correctAnswers() {
         // Gets text after questions
         for (int i = 0; i < _questions; i++) {
             correctAnswer[i] = temp[i].substring(Integer.parseInt(index[i]));
+            //    System.out.println(correctAnswers[i]);
         }
-
         // Gets rid of text before
         for (int i = 0; i < _questions; i++) {
             correctAnswer[i] = correctAnswer[i].substring(20);
+            System.out.println(correctAnswer[i]);
+            System.out.println();
         }
 
-        // Gets index of next quotation mark
         for (int i = 0; i < _questions; i++) {
             index[i] = String.valueOf(correctAnswer[i].indexOf("\""));
         }
 
-        //Copy to temp
-        for (int i = 0; i < _questions; i++) {
-            temp[i] = correctAnswer[i];
-        }
-
-        // Assigns correct answer by itself into array
-        for(int i = 0; i < _questions; i++){
-            correctAnswer[i] = correctAnswer[i].substring(0, Integer.parseInt(index[i]));
-            System.out.println("Correct Answer " + (i + 1) +": " + correctAnswer[i]);
-            System.out.println();
-        }
-        // Sets leftover substring into this array
-        for (int j = 0; j < _questions; j++) {
-            incorrectAnswer[j] = temp[j].substring(Integer.parseInt(index[j]));
+        for(int j = 0; j < _questions; j++){
+            correctAnswer[j] = correctAnswer[j].substring(Integer.parseInt(index[j]));
+            //System.out.println(correctAnswer[j]);
+            //System.out.println();
         }
         return correctAnswer;
-    }
-    public static String[] incorrectAnswers() {
-        // Gets text after correct answer
-        for (int i = 0; i < _questions; i++) {
-            incorrectAnswer[i] = temp[i].substring(Integer.parseInt(index[i]));
-        }
-        // Remove text in front of incorrect answers
-        for (int i = 0; i < _questions; i++) {
-            incorrectAnswer[i] = incorrectAnswer[i].substring(23);
-        }
-
-        // Gets index of next quotation mark
-        for (int i = 0; i < _questions; i++) {
-            index[i] = String.valueOf(incorrectAnswer[i].indexOf("]"));
-        }
-
-        // Assigns incorrect answer by itself into array
-        for(int i = 0; i < _questions; i++){
-            incorrectAnswer[i] = incorrectAnswer[i].substring(0, Integer.parseInt(index[i]));
-            System.out.println("incorrect Answers for Question " + (i + 1) +": " + incorrectAnswer[i]);
-            System.out.println();
-        }
-        return incorrectAnswer;
     }
 
     public static void main(String[] args) {
         questions();
         correctAnswers();
-        incorrectAnswers();
     }
 
 }
--- utils/APICode/Questions.java
+++ utils/APICode/Questions.java
@@ -1,60 +1,85 @@
 package APICode;
 
 /*
- * Separates questions from query.
- * Updated 4-19-21
+ * Separates questions from query. Updated
+ * Updated 4-13-21
  *
  * @author Sengthida Lorvan
  */
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Questions extends API {
-    //arrays to store parsed parts
+
+    public static final String _BASEURL = "https://opentdb.com/api.php?";
     public static String[] questions = new String[_questions];
     public static String[] Question = new String[_questions];
     public static String[] index = new String[_questions];
     public static String[] temp = new String[_questions];
 
+
     public static String[] questions() {
-        makeAPICall();
-
+        String _callAction1 = "amount=";
+        String _callAction2 = "&category=";
+        String _callAction3 = "&difficulty=";
+        String _urlString = _BASEURL + _callAction1 + _questions + _callAction2 + _category + _callAction3 + _difficulty;
+        URL _url;
         try {
-            // prints out questions
-            var question = _obj.getJSONArray("results").opt(0);
+            _url = new URL(_urlString);
+            HttpURLConnection connect = (HttpURLConnection) _url.openConnection();
+            connect.setRequestMethod("GET");
+            int _status = connect.getResponseCode();
+            if (_status != 200) {
+                System.out.println("Error: Could not get questions: " + _status);
+            } else {
+                BufferedReader _input = new BufferedReader(new InputStreamReader(connect.getInputStream()));
+                String _inputLine;
+                StringBuilder content = new StringBuilder();
+                while ((_inputLine = _input.readLine()) != null) {
+                    content.append(_inputLine);
+                }
+                _input.close();
+                connect.disconnect();
 
-            // Get questions
-            for (int i = 0; i < _questions; i++) {
-                question = _obj.getJSONArray("results").opt(i);
-                questions[i] = question.toString();
-            }
+                JSONObject _obj = new JSONObject(content.toString());
 
-            // Remove text in front of questions
-            for (int i = 0; i < _questions; i++) {
-                Question[i] = questions[i].substring(33);
-            }
+                // prints out questions
+                var question = _obj.getJSONArray("results").opt(0);
 
-            // Gets index of next quotation mark
-            for (int i = 0; i < _questions; i++) {
-                index[i] = String.valueOf(Question[i].indexOf("\""));
-            }
 
-            //Copy to temp
-            for (int i = 0; i < _questions; i++) {
-                temp[i] = Question[i];
-            }
-
-            System.out.println();
-
-            // Assigns questions by itself to array
-            for (int i = 0; i < _questions; i++) {
-                Question[i] = Question[i].substring(0, Integer.parseInt(index[i]));
-                System.out.println("Question " + (i + 1) +": " + Question[i]);
+                // Get questions
+                for (int i = 0; i < _questions; i++) {
+                    question = _obj.getJSONArray("results").opt(i);
+                    questions[i] = question.toString();
+                }
+                // Remove text in front of questions
+                for (int i = 0; i < _questions; i++) {
+                    Question[i] = questions[i].substring(33);
+                }
+                // Gets index of next quotation mark
+                for (int i = 0; i < _questions; i++) {
+                    index[i] = String.valueOf(Question[i].indexOf("\""));
+                }
+                //Copy to temp
+                for (int i = 0; i < _questions; i++){
+                    temp[i] = Question[i];
+                }
                 System.out.println();
+                // Assigns questions by itself to array
+                for (int i = 0; i < _questions; i++) {
+                    Question[i] = Question[i].substring(0, Integer.parseInt(index[i]));
+                    System.out.println("Question:" + Question[i]);
+                    System.out.println();
+                }
             }
-
         } catch (Exception _ex) {
             System.out.println("Error: " + _ex);
         }
-
         return Question;
     }
 }
