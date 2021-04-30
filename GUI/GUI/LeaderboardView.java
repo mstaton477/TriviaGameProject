9b67b1084699cb45cda0a208f0a72ab6c204e944
Patrick Ksor <71914119+Patar-spec@users.noreply.github.com>
2021-04-30 15:49:27 -0400

    Revert "Revert "Revert "leaderboardworks"""
    This reverts commit d836909dfd4a2ac549dd636a837f1e66b532d4ec.

--- GUI/GUI/LeaderboardView.java
+++ GUI/GUI/LeaderboardView.java
@@ -19,21 +19,46 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+<<<<<<< Updated upstream
+=======
 import javafx.scene.control.Label;
+>>>>>>> Stashed changes
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
-import java.sql.SQLException;
-import java.sql.Timestamp;
+<<<<<<< Updated upstream
 import java.time.LocalDate;
 import java.util.ResourceBundle;
+
+public class LeaderboardView implements Initializable {
+
+
+    //Sets up fx:ids to link to the fxml
+    @FXML private TableView<Leaderboard> _tableView;
+    @FXML private TableColumn<Leaderboard, String> _nameColumn;
+    @FXML private TableColumn<Leaderboard, Integer> _scoreColumn;
+    @FXML private TableColumn<Leaderboard, String> _categoryColumn;
+    @FXML private TableColumn<Leaderboard, String> _difficultyColumn;
+    @FXML private TableColumn<Leaderboard, Integer> _lengthColumn;
+    @FXML private TableColumn<Leaderboard, LocalDate> _datePlayedColumn;
+
+    ObservableList<Leaderboard> _observableList = FXCollections.observableArrayList();
+
+
+/**
+=======
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ResourceBundle;
+
 
 
 public class LeaderboardView implements Initializable {
     /**
+>>>>>>> Stashed changes
      * This sends the user back to the initial view of the application.
      * @param event
      * @throws IOException
@@ -48,8 +73,21 @@ public class LeaderboardView implements Initializable {
         window.setScene(backButtonScene);
         window.show();
     }
+<<<<<<< Updated upstream
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        _nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+        _scoreColumn.setCellValueFactory(new PropertyValueFactory<>("score"));
+        _categoryColumn.setCellValueFactory(new PropertyValueFactory<>("category"));
+        _difficultyColumn.setCellValueFactory(new PropertyValueFactory<>("difficulty"));
+        _lengthColumn.setCellValueFactory(new PropertyValueFactory<>("length"));
+        _datePlayedColumn.setCellValueFactory(new PropertyValueFactory<>("datePlayed"));
 
+        _tableView.setItems(_observableList);
+    }
+
+=======
     @FXML
     private Label label;
     @FXML
@@ -90,5 +128,6 @@ public class LeaderboardView implements Initializable {
         LeaderBoard.setItems(null);
         LeaderBoard.setItems(data);
     }
+>>>>>>> Stashed changes
 }
 
