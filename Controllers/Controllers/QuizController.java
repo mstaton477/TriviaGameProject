7111481f9bd762a45fdbb64edee95d2a2c101374
mstaton477 <mlstaton@uncg.edu>
2021-04-29 21:10:18 -0400

    FIXED API ARRAY

--- Controllers/Controllers/QuizController.java
+++ Controllers/Controllers/QuizController.java
@@ -19,19 +19,20 @@ import static Models.Quiz.*;
 
 public class QuizController {
     public static String[] _questionArray;
-    public static  String[] _answerArray;
+    public static String[] _answerArray;
 
-    public static void callCheckIfCorrect(){
+    public static void callCheckIfCorrect() {
         Quiz.checkIfCorrect();
     }
 
-    public static void callCalculateScore(){
+    public static void callCalculateScore() {
         Quiz.calculateScore();
     }
 
 
-
-    public static void callAPIRun(){Quiz.APIRun();}
+    public static void callAPIRun() {
+        Quiz.APIRun();
+    }
 
 
     // ================================ GETTERS ====================================
@@ -39,59 +40,58 @@ public class QuizController {
     //gets the player answer choice from the quiz view to use in the
     // check if correct method
 
-    public static String getChoice(){
+    public static String getChoice() {
         return QuizView._choice;
     }
 
-    public static String getCategoryChoice(){
+    public static String getCategoryChoice() {
         return SetUpQuestions._categoryChoice;
     }
 
-    public static String getGameLength(){
-        return SetUpQuestions._lengthChoice;
+    public static int getGameLength() {
+        return Player.getGameLength();
     }
 
-    public static String getGameDifficulty(){
-        return SetUpQuestions._difficultyChoice;
+    public static String getGameDifficulty() {
+        return Player.getGameDifficulty();
     }
 
-    public static int getCategory(){
+    public static int getCategory() {
         return Player.getCategory();
     }
 
-    public static String getPlayerName(){
-            return Player.getPlayerName();
+    public static String getPlayerName() {
+        return Player.getPlayerName();
     }
 
-    public static String[] getQuestionsArray(){
+    public static String[] getQuestionsArray() {
         Quiz.getQuestionArray();
-
 
         return _questionArray = questionArray;
     }
-    public static String[] getAnswerArray(){
+
+    public static String[] getAnswerArray() {
         Quiz.getAnswerArray();
-
-
-        return _answerArray = answerArray;}
+        return _answerArray = answerArray;
+    }
 
     // ================================ SETTERS ====================================
 
-    public static void setGameLength(String _gameLength) { Quiz.convertGameLengthStringToIntAndSet(_gameLength);
+    public static void setGameLength(String _gameLength) {
+        Quiz.convertGameLengthStringToIntAndSet(_gameLength);
     }
 
-    public static void setCategory(String _category){
+    public static void setCategory(String _category) {
         Quiz.convertCategoryStringToIntAndSet(_category);
     }
 
 
-
-    public static void setGameDifficulty(String _gameDifficulty){
-        Quiz.convertDifficultyToLowercaseAndSet(_gameDifficulty);
+    public static void setGameDifficulty(String _gameDifficulty) {
+        Quiz.setGameDifficulty(_gameDifficulty);
     }
 
 
-    public static void setPlayerName(String _playerName){
-       Player.setPlayerName(_playerName);
+    public static void setPlayerName(String _playerName) {
+        Player.setPlayerName(_playerName);
     }
 }
